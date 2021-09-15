f14cd43681802adda2def26a6efb0973935b255e
Akshit Mangotra <57808363+Akshit6828@users.noreply.github.com>
2021-09-16 01:23:36 +0530

    Updated Olympics ranking java solution

---
+++ Codechef/Olympics_Ranking.java
@@ -0,0 +1,17 @@
+public class Olympics_Ranking{
+
+     public static void main(String []args){
+     Scanner s = new Scanner(System.in);
+     int a =s.nextInt();
+    int b =s.nextInt();
+    int c =s.nextInt();
+    int d =s.nextInt();
+    int e =s.nextInt();
+    int f =s.nextInt();
+      
+      if(a+b+c>f+d+e)
+        System.out.println("1);
+        else
+        System.out.println("2);
+     }
+}
